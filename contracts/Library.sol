23926dcee973bbd0b679b8f91c23e7f51f9e969d
Atanas Georgiev <atanas.georgiev@bul-si.bg>
2021-12-05 18:49:37 +0200

    add hardhat project structure

---
+++ contracts/Library.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./Ownable.sol";
+
+contract Library is Ownable {
+
+    event NewBookAdded(Book book);
+    event NewCopyOfBookAdded(Book book);
+    event BookBorrowed(Book book);
+    event BookReturned(Book book);
+
+    struct Book {
+        string isbn;
+        string name;
+        string author;
+        uint copies;
+    }
+
+    Book[] public books;
+
+    function _addBook(string memory _isbn, string memory _name, string memory _author, uint _copies) public onlyOwner {
+        uint numberOfBooks = books.length;
+
+        bool isNewBook = false;
+
+        if (numberOfBooks == 0) {
+           isNewBook = true;
+        }
+        
+        for (uint i = 0; i < numberOfBooks; i++) {
+           Book storage book = books[i];
+           if (keccak256(abi.encodePacked(book.isbn)) == keccak256(abi.encodePacked(_isbn))) {
+               // book alreadey exists => inc number of copies
+               book.copies++;
+               emit NewCopyOfBookAdded(book);
+           }
+        }
+
+        if (isNewBook) {
+            _addNewBook(Book(_isbn, _name, _author, _copies));
+        }
+    }
+
+    function _addNewBook(Book memory book) private {
+        books.push(book);
+        emit NewBookAdded(book);
+    }
+
+    function _borrowBook(string memory _isbn) public onlyOwner {
+        uint numberOfBooks = books.length;
+        
+        for (uint i = 0; i < numberOfBooks; i++) {
+           Book storage book = books[i];
+           if (keccak256(abi.encodePacked(book.isbn)) == keccak256(abi.encodePacked(_isbn)) 
+                && book.copies > 1)  {
+               book.copies--;
+               emit BookBorrowed(book);
+           }
+        }
+    }
+
+    function _returnBook(string memory _isbn) public onlyOwner {
+        uint numberOfBooks = books.length;
+        
+        for (uint i = 0; i < numberOfBooks; i++) {
+           Book storage book = books[i];
+           if (keccak256(abi.encodePacked(book.isbn)) == keccak256(abi.encodePacked(_isbn)))  {
+               book.copies++;
+               emit BookReturned(book);
+           }
+        }
+    }
+}
